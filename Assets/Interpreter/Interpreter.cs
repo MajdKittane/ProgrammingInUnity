32f9b368e24f66fd7e05d3967ee3dd63b94b99e7
MajdKittane <121731267+MajdKittane@users.noreply.github.com>
2023-09-25 21:04:41 +0300

    Experimenting, while trying to make a good demo

--- Assets/Interpreter/Interpreter.cs
+++ Assets/Interpreter/Interpreter.cs
@@ -3,45 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using MyInterpreter;
 
-public class Interpreter : MonoBehaviour
+public abstract class Interpreter : MonoBehaviour
 {
-    public GameObject prefab;
-    [SerializeField]
-    TMPro.TMP_InputField code;
-    [SerializeField]
-    GameObject outputTransform;
-    bool done = false;
-    int num = 0;
-    Vector3 end;
-    float elapsedTime = 0f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void RunCode()
-    {
-        done = false;
-        elapsedTime = 0f;
-        var lexer = new Lexer(code.text);
-        var parser = new Parser(lexer);
-        var program = parser.ParseProgram();
-        Environment env = new Environment();
-        Evaluator.Eval(program, env);
-
-        num = int.Parse(env.store["num"].Inspect());
-        done = true;
-        for (int i = 0; i<num; i++)
-        {
-            Instantiate(prefab, new Vector3(0f, 100f, 0f), Quaternion.identity);
-        }
-    }
 }
